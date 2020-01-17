1411bd740abf3c0c9f00117a6ea0cb85dcb49726
Manzi Guevara <manziguevara@gmail.com>
2020-01-17 10:26:46 +0200

    ft(backend): admin can add a category [Finishes #170754457]

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -10,11 +10,7 @@ import isAdmin from '../middlewares/isAdmin';
 
 const router = Router();
 
-<<<<<<< HEAD
 router.post('/auth/login', AuthValidator.signinValidator, asyncErrorHandler(UserController.signIn));
-=======
-router.post('/signin', AuthValidator.signinValidator, asyncErrorHandler(UserController.signIn));
 router.post('/categories', isLogged, isAdmin, categoryValidation.createCategory, asyncErrorHandler(categoryController.create));
->>>>>>> ft(backend): admin can add a category [Finishes #170754457]
 
 export default router;
